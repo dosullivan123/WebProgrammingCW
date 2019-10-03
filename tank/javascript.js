5cd1386f273fb48541f4296527e513f1003814f3
lachlan mackay <lachlanmackay@lachlans-MacBook-Pro.local>
2019-10-03 14:30:54 +0100

    basic website structure

---
+++ tank/javascript.js
@@ -0,0 +1,8 @@
+function expand(dis) {
+    if (document.getElementById("overlay").style.display = "none" == true) {
+      document.getElementById("overlay").style.display = "block";
+    }
+    else {
+      document.getElementById("overlay").style.display = "none";
+    }
+  }
