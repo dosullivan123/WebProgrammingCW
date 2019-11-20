8a1dd80297893addd928d63b8d7942cc9f4604c8
Stuart McCafferty <sm342@hw.ac.uk>
2019-11-20 14:17:06 +0000

    Finished bullets
    bullets fire on mouse click and go towards mouse

--- Web Game Project/app.js
+++ Web Game Project/app.js
@@ -38,12 +38,23 @@ var Player = function(id){
     self.pressingLeft = false;
     self.pressingUp = false;
     self.pressingDown = false;
+    self.pressingAttack = false;
+    self.mouseAngle = 0;
     self.maxSpd = 10;
 
     var super_update = self.update;
     self.update = function(){
         self.updateSpd();
         super_update();
+
+        if(self.pressingAttack){
+            self.shootBullet(self.mouseAngle)
+        }
+    }
+    self.shootBullet = function(angle){
+      var b = Bullet(angle);
+      b.x = self.x;
+      b.y = self.y;
     }
 
 
@@ -77,6 +88,10 @@ Player.onConnect = function(socket){
             player.pressingUp = data.state;
         else if(data.inputId === 'down')
             player.pressingDown = data.state;
+        else if(data.inputId === 'attack')
+            player.pressingAttack = data.state;
+        else if(data.inputId === 'mouseAngle')
+            player.mouseAngle = data.state;
     });
 }
 Player.onDisconnect = function(socket){
@@ -117,9 +132,6 @@ var Bullet = function(angle){
 Bullet.list = {};
 
 Bullet.update = function(){
-    if(Math.random() < 0.1){
-        Bullet(Math.random()*360);
-    }
 
     var pack = [];
     for(var i in Bullet.list){
